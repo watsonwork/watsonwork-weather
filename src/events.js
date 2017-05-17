454b22c3504212612dcd9a7bf302495f744ac9a9
Cyrus Jia <cyrus.jia@ibm.com>
2017-05-17 15:55:46 -0700

    Use Watson Conversation intents instead of actions
    Convert the application logic to use Watson Conversation intents instead
    of actions (as Watson Work action identification and the handling of
    action dialogs will be implemented in a different sample app.)
    
    The related changes to the Watson Conversation workspace (watson.json)
    will be committed separately.
    
    Signed-off-by: Jean-Sebastien Delfino <jsdelfino@gmail.com>

--- src/events.js
+++ src/events.js
@@ -28,8 +28,8 @@ const callback = (evt, appId, info, annotation, token, cb) => {
   });
 };
 
-// Return the action identified in an annotation event
-export const onAction = (evt, appId, token, cb) => {
+// Return the intent identified in an annotation event
+export const onIntent = (evt, appId, token, cb) => {
   // Check for a focus annotation
   if(evt.type === 'message-annotation-added' &&
     evt.annotationType === 'message-focus') {
@@ -37,29 +37,10 @@ export const onAction = (evt, appId, token, cb) => {
     // Call back with any action found on the focus annotation
     const focus = JSON.parse(evt.annotationPayload);
     if(focus.applicationId === appId) {
-      const action = focus.actions && focus.actions[0];
-      if(action) {
-        log('Idenfified action %s', action);
-        callback(evt, appId, action, focus, token, cb);
-      }
-    }
-  }
-};
-
-// Return the next step in an action, determined from a user input
-export const onActionNextStep = (evt, appId, token, cb) => {
-  // Check for a focus annotation
-  if(evt.type === 'message-annotation-added' &&
-    evt.annotationType === 'message-focus') {
-
-    // Call back with any action next step found on the focus annotation
-    const focus = JSON.parse(evt.annotationPayload);
-    if(focus.applicationId === appId) {
-      const payload = JSON.parse(focus.payload);
-      const next = (payload.actionNextSteps || [])[0];
-      if(next) {
-        log('Idenfified action next step %s', next);
-        callback(evt, appId, next, focus, token, cb);
+      const intent = focus.lens;
+      if(intent) {
+        log('Idenfified intent %s', intent);
+        callback(evt, appId, intent, focus, token, cb);
       }
     }
   }
@@ -70,7 +51,6 @@ export const onEntities = (evt, appId, token, cb) => {
   // Check for an entities annotation
   if(evt.type === 'message-annotation-added' &&
     evt.annotationType === 'message-nlp-entities') {
-
     // Call back with any recognized entities
     const nlp = JSON.parse(evt.annotationPayload);
     if(nlp.entities.length) {
@@ -79,4 +59,3 @@ export const onEntities = (evt, appId, token, cb) => {
     }
   }
 };
-
